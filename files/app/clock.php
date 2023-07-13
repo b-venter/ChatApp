663da40fd153a67f8a519daa2f2234018bacc275
b-venter <52171108+b-venter@users.noreply.github.com>
2023-07-13 10:38:16 +0200

    Delete files directory

--- files/app/clock.php
+++
@@ -1,4 +0,0 @@
-<?php
-$date = new DateTime("now", new DateTimeZone('Africa/Windhoek') );
-echo $date->format('Y-m-d H:i:s');
-?>
--- files/vm/download link.md
+++
@@ -1,2 +0,0 @@
-
-https://drive.google.com/open?id=14oaHy966eJ6z2FU7oG0nk4jSDRwJZbdt
